ca828e44b3b8b90120b176451dffbeaca5eaf471
wodzynski <macko5395@gmail.com>
2019-01-23 18:38:21 +0100

    Added show page and mongoDB database

--- app.js
+++ app.js
@@ -1,42 +1,100 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express     = require('express'),
+      app         = express(),
+      bodyParser  = require('body-parser'),
+      mongoose    = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-const campgrounds = [
-  {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&auto=format&fit=crop&w=649&q=80'},
-  {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
-  {name: 'Mountain Goat\'s rest', image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
-  {name: 'Yukon Valley', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&auto=format&fit=crop&w=649&q=80'},
-  {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
-  {name: 'Mountain Goat\'s rest', image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
-  {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&auto=format&fit=crop&w=649&q=80'},
-  {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
-  {name: 'Mountain Goat\'s rest', image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'}
-]
+// SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: 'Granite Hill',
+//     image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+//     description: 'This is a huge granit hill, no bathrooms, no water, beautiful granite!'
+//   }, (err, campground) => {
+//     if(err){
+//       console.log(err);
+//     } else {
+//       console.log('NEWLY CREATED CAMPGROUND: ');
+//       console.log(campground);
+//     }
+// });
+
+
+// const campgrounds = [
+//   {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&auto=format&fit=crop&w=649&q=80'},
+//   {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
+//   {name: 'Mountain Goat\'s rest', image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
+//   {name: 'Yukon Valley', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&auto=format&fit=crop&w=649&q=80'},
+//   {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
+//   {name: 'Mountain Goat\'s rest', image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
+//   {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&auto=format&fit=crop&w=649&q=80'},
+//   {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'},
+//   {name: 'Mountain Goat\'s rest', image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'}
+// ]
 
 app.get('/', (req,res) => {
   res.render('landing');
 });
 
+//INDEX - show all campgrounds
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', {campgrounds: campgrounds});
+  //GET all campgrounds from DB
+  Campground.find({}, (err, allCampgrounds) => {
+    if(err){
+      console.log(err);
+    } else {
+      res.render('index', {campgrounds:allCampgrounds});
+    }
+  });
+  // res.render('campgrounds', {campgrounds: campgrounds});
 });
 
+//CREATE - add new campground to DB
 app.post('/campgrounds', (req, res) => {
   //get data from form and add to campground array
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  //redirect back to campgrounds page
-  res.redirect('/campgrounds');
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
+  // Create a new campground and save to the DB
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if(err){
+      console.log(err);
+    } else {
+      //redirect back to campgrounds page
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
+//NEW - show form to create new campground
+//executed before INFO because of the order
 app.get('/campgrounds/new', (req, res) => {
   res.render('new.ejs');
+});
+
+//INFO - show info about one campground
+app.get('/campgrounds/:id', (req, res) => {
+  //find the campground with provided ID
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if(err){
+      console.log(err);
+    }else {
+      //render show template with that campground
+      res.render('show', {campground: foundCampground});
+    }
+  });
 });
 
 app.listen(3000, process.env.IP, () => {
