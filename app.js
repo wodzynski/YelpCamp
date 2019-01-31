cfc74bea3626291446165b57d5398e50220055f2
wodzynski <macko5395@gmail.com>
2019-01-31 13:17:24 +0100

    Added edit, update and destroy routes and implemented it in the app.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express       = require('express'),
       mongoose      = require('mongoose'),
       passport      = require('passport'),
       LocalStrategy = require('passport-local'),
+      methodOverride = require('method-override'),
       Campground    = require('./models/campground'),
       Comment       = require('./models/comment'),
       User          = require('./models/user'),
@@ -17,6 +18,8 @@ const commentRoutes     = require('./routes/comments'),
 mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 // seedDB(); // seed the database
 
 // PASSPORT CONFIGURATION
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -56,6 +56,41 @@ router.get('/:id', (req, res) => {
   });
 });
 
+// EDIT CAMPGROUND ROUTE
+router.get('/:id/edit', (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if(err){
+      res.redirect('/campgrounds');
+    } else {
+      res.render('campgrounds/edit', {campground: foundCampground});
+    }
+  })
+})
+
+// UPDATE CAMPGROUND ROUTE
+router.put('/:id', (req, res) => {
+  // find and update the correct campground
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+    if(err){
+      res.redirect('/campgrounds');
+    } else {
+      // redirect to edited show page
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
+});
+
+// DESTROY CAMPGROUND ROUTE
+router.delete('/:id', (req, res) => {
+  Campground.findByIdAndRemove(req.params.id, err => {
+    if(err){
+      res.redirect('/campgrounds');
+    } else {
+      res.redirect('/campgrounds');
+    }
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
