a297dfc423dbd13f0126364432223dfeb61d7f55
wodzynski <macko5395@gmail.com>
2020-01-07 19:41:32 +0100

    add dburl in environment var

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ const commentRoutes     = require('./routes/comments'),
       campgroundRoutes  = require('./routes/campgrounds'),
       indexRoutes       = require('./routes/index');
 
-//mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
-mongoose.connect('mongodb+srv://wodzynski:mw125422@cluster0-cw5gp.mongodb.net/yelp_camp?retryWrites=true&w=majority', { useNewUrlParser: true });
+require('dotenv').config({path: __dirname + '/.env'});
+
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb+srv://wodzynski:mw125422@cluster0-cw5gp.mongodb.net/yelp_camp?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
